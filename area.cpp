5d65b7f6ac531acc7e46eb27219f9099877959de
alduma <37373309+alduma@users.noreply.github.com>
2018-03-21 09:57:19 -0500

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int x;
+    float y;
+    cin>>x;
+    cin>>y;
+    int z=x+y;
+    int t=x-y;
+    int u=x*y;
+    float v=x/y;
+    int q;
+    float e;
+    cout<<"valor de radio"<<endl;
+
+    cin>>q;
+    e=3.14*(q*q);
+
+    cout<<"the first number is:"<<x<<endl;
+    cout<<"the second number is:"<<y<<endl;
+    cout <<"the sum is"<<z << endl;
+    cout <<"la resta es:"<<t << endl;
+    cout <<"la multiplicacion es"<<u << endl;
+    cout <<"la division es"<<v << endl;
+    cout <<"area del circulo"<<e << endl;
+    cout<<".....\n.....\n.....\n.....\t.....\n.     .\t.....\t....."<<endl;
+    cout<<".\t.\t.\t."<<endl;
+
+
+
+    return 0;
+}
+
