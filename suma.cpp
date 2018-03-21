f237c52c887048970ba2e8d634f6aa41445131e6
alduma <37373309+alduma@users.noreply.github.com>
2018-03-21 08:56:24 -0500

    Add files via upload

---
+++ suma.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    int x=4,y=5,z=x+y,t=x-y,u=x*y;
+    cout<<"the first number is:"<<x<<endl;
+    cout<<"the second number is:"<<y<<endl;
+    cout <<"the sum is"<<z << endl;
+    cout <<"la resta es:"<<t << endl;
+    cout <<"la multiplicacion es"<<u << endl;
+    return 0;
+}
